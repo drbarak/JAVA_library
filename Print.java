a0210375eaa425347ac1be149279e299dbc5481a
Zvi Barak <drbarak@talkie.co.il>
2024-12-19 08:00:03 +0200

    Add files via upload

---
+++ Print.java
@@ -0,0 +1,70 @@
+import java.util.Arrays;
+/**
+ * Class Print to allow to use method p() to print messages instead of System.out...
+ * After making changes and compiling, need to create a Jar file names MyLib.jar
+ * 
+ * The class is automatically use in each new project once it is set as a
+ * user library from Tools->preferences for all projects
+ *
+ * @author (צבי ברק)
+ * @version (02.12.2024)
+ */
+public class Print
+{
+    public static void p(String s)
+    {
+        System.out.println(s);
+    }
+    public static void p(int s)
+    {
+        p(" " + s);
+    }
+    public static void p(boolean n)
+    {
+        p("" + n);
+    }
+    public static void p(int n1, int n2)
+    {
+        p("" + n1 + ", " + n2);
+    }
+    public static void p(int n1, int n2, int n3)
+    {
+        p("" + n1 + ", " + n2 + ", " + n3);
+    }
+    public static void p(int n1, int n2, int n3, int n4)
+    {
+        p("" + n1 + ", " + n2 + ", " + n3 + ", " + n4);
+    }
+    public static void p(int[] arr)
+    {
+        p("", arr);
+    }
+    public static void p(char[] arr)
+    {
+        p("", arr);
+    }
+    public static void p(String s, int[] arr)
+    {
+        p(s + Arrays.toString(arr));
+    }
+    public static void p(String s, char[] arr)
+    {
+        p(s + Arrays.toString(arr));
+    }
+    public static void p(String s, int n)
+    {
+        p(s + ", " + n);
+    }
+    public static void p(int[][] arr)
+    {
+        // not using Arrays.deepToString(arr)) becuse print all lines in one row
+        for (int i = 0; i < arr.length; i++)
+            p(arr[i]);
+    }
+    public static void p(char[][] arr)
+    {
+        // not using Arrays.deepToString(arr)) becuse print all lines in one row
+        for (int i = 0; i < arr.length; i++)
+            p(arr[i]);
+    }
+}
---
+++ SiteMap.java
@@ -0,0 +1,38 @@
+
+/**
+ * @author (Zvi Barak)
+ * @version (a version number or a date)
+ * 
+ * Folder Library includes:
+ *  Point,
+ *  Print
+ *  
+ * Folder תרגילים נוספים includes:
+ *      -- using Recursion --
+ *  Recursion.permutation,
+ *  Recursion.powerSet,
+ *  Recursion.findInterleavings() of 2 Strings,
+ *  Recursion.factorial
+ *   -- using loops (while or for) -- 
+ *  EditDistance.editDistance Approximated String Matching,
+ *  ConwayGameOfLife - leave or die based on neighbours,
+ *  LastMatch - pick matches from the table usinl none,
+ *  Disco - turn on/off bulbs to see if set 1 of bulbs can become set 2,
+ *  howMany - number of times a subsring appear in a string,
+ *  longestArrow - find longest subset of integers in acsending order
+ *  
+ *  Folder ממן 14 includes: -- using Tree and loops (while or for)--
+ *      MatrixList.getData_i_j,
+ *      MatrixList.setData_i_j,
+ *      MatrixList.findMax - largest integer,
+ *      MatrixList.howManyX - how many times X in matrix where each row and 
+ *          columns are in acsending order, in an efficient way
+ *      
+ *  Folder ממן 13 includes:  -- using Recursion --
+ *      Ex13.extreme - find largest number in all possible paths from
+ *          top left to bottom right, moving up/down/left/right
+ *      Ex13.specialArr - arrange array with less and more neighbours
+ *      Ex13.first
+ *      Ex13.longestNearlyPalindrome
+ *  
+ */
